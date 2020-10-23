03500cf1bf043e08e609e519a754a8ca9ba515cc
mikenavarroro <mike.navarroro@gmail.com>
2020-10-23 16:11:16 -0500

    Examen 1

---
+++ Examen.r
@@ -0,0 +1,11 @@
+#Pregunta 1 inciso b
+data = c(20, 30, 10, 40, 140, 0, 10, 20, 30)
+boxplot(data, horizontal = T)
+
+#Pregunta 2 inciso b
+data1 = c(0, 0.5, 1, 2, 3, 5, 7, 9, 12, 15, 18, 22)
+stem(data1)
+
+#Pregunta 2 inciso c
+data2 = c(100, 42, 14, 7.5, 0.4, 0.11, 0.09, 0.05, 0.005, 0.002, 0.0001, 0)
+boxplot(data2, horizontal = T)
